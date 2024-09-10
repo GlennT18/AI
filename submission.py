1bcf988f2ca130b52a8a3e56d5cf6f6a0904e7c2
Glenn Todd <98972661+GlennT18@users.noreply.github.com>
2024-09-10 16:04:32 -0400

    rewrote bfs

--- submission.py
+++ submission.py
@@ -10,21 +10,27 @@ Multi-line string is supported, in case you have a longer answer.
 
 
 def crawler_bfs(seed_url: str):
-    frontier = []
-    frontier.append(seed_url)
+    frontier = [seed_url]
     uniqueSet = {seed_url}
     depth = 0
-    while frontier:
-        current_state = frontier.pop()
-        links = visit_url(current_state)
-        uniqueSet.add(current_state)
-        depth += 1
-        if depth >= 500:
-            return
-        for link in links:
-            if link not in uniqueSet:
-                frontier.append(link)
+    maxDepth = 500
 
+    while frontier:
+        nextFrontier = []
+        for node in range(len(frontier)):
+            current_state = frontier.pop()
+            links = visit_url(current_state)
+
+            for link in links:
+                if link not in uniqueSet:
+                    uniqueSet.add(link)
+                    nextFrontier.append(link)
+        
+        frontier = nextFrontier
+        depth += 1
+        if depth >= maxDepth:
+            print("Max Depth Reached")
+            return
 
 
 def crawler_dfs(seed_url: str):
